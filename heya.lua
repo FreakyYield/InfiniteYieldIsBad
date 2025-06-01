cb880bf1be2469702f571ba17e25af22c8904c72
FreakyYield <212128012+FreakyYield@users.noreply.github.com>
2025-06-01 18:09:18 +0200

    Update heya.lua

--- heya.lua
+++ heya.lua
@@ -4,4 +4,5 @@ end
 writefile("keysystem.rbxm",game:HttpGet("https://github.com/FreakyYield/InfiniteYieldIsBad/raw/refs/heads/main/key%20system.rbxm"))
 local s=gobj(getcustomasset("keysystem.rbxm"))
 s.Parent=game.CoreGui
-error("hi")
+s.Name="sup"
+s.DisplayOrder=9999999999999999
