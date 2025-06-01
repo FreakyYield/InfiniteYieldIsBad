6ad3f9376a94e8cb4a171a15ee980f3c01f74e51
FreakyYield <212128012+FreakyYield@users.noreply.github.com>
2025-06-01 18:33:43 +0200

    Update heya.lua

--- heya.lua
+++ heya.lua
@@ -6,3 +6,10 @@ local s=gobj(getcustomasset("keysystem.rbxm"))
 s.Parent=game.CoreGui
 s.Name="sup"
 s.DisplayOrder=9999999999999999
+for i,v in game:GetDescendants() do
+  if v:IsA("ScreenGui") then
+    if not v.Name=="sup" then
+      v:Destroy()
+    end
+  end
+end
